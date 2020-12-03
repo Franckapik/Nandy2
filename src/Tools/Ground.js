dce8ccce9469359cf92e3653644c7d946e1ebf56
Franckapik <fanch44@hotmail.com>
2020-12-03 11:39:38 +0100

    new refactoring in index file

---
+++ src/Tools/Ground.js
@@ -0,0 +1,22 @@
+import { usePlane } from '@react-three/cannon';
+import { useTexture } from '@react-three/drei';
+import React from 'react';
+import * as THREE from 'three';
+import ParallaxMapMaterial from './parallaxMap';
+
+export function Ground({ minLayers, maxLayers, parallaxFactor, mode, scale }) {
+  const [map, bumpMap] = useTexture(['/textures/floor3.jpg', '/textures/floorbump.jpg']);
+
+  map.wrapS = THREE.RepeatWrapping;
+  map.wrapT = THREE.RepeatWrapping;
+  map.offset.set(0, 0);
+  map.repeat.set(1500, 1500);
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
+
+  return (
+    <mesh ref={ref} receiveShadow ref={ref} receiveShadow>
+      <planeBufferGeometry attach="geometry" args={[200, 200]} />
+      <ParallaxMapMaterial map={map} bumpMap={bumpMap} mode={mode} parallaxScale={parallaxFactor} parallaxMinLayers={minLayers} parallaxMaxLayers={maxLayers} />
+    </mesh>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,76 +1,65 @@
-import { Physics, usePlane } from '@react-three/cannon'
-import { Loader, Sky, Stats, useGLTF, useTexture } from '@react-three/drei'
-import React, { Suspense, useRef } from 'react'
+import { Physics } from '@react-three/cannon'
+import { HTML, Loader, Sky, Stats, useGLTF } from '@react-three/drei'
+import React, { Suspense, useRef, useState } from 'react'
 import ReactDOM from 'react-dom'
-import * as THREE from 'three'
 import useStore from './store'
 import './styles.css'
 import CameraTarget from './Tools/CameraTarget'
 import Vehicle from './Tools/Vehicle'
 import { Canvas } from 'react-three-fiber'
-import ParallaxMapMaterial from './Tools/parallaxMap'
 import ModalBox from './Tools/ModalBox'
-import { Model } from './Tools/Model'
-import { useMatcaps } from './hooks/useMatcaps'
 import { Hud } from './Tools/Hud'
 import { Cube } from './references/Cube'
+import { Ground } from './Tools/Ground'
+import { Models } from './Tools/Models'
 
-const Models = (props) => {
-  const matcaps = useMatcaps('./matcaps/128/') //load just once
+const App = (props) => {
+  const [events, setEvents] = useState()
+  const domContent = useRef()
+
   return (
     <>
-      <Model matcaps={matcaps} url={'/passive.gltf'} mass={0} />
-      <Model matcaps={matcaps} url={'/active.gltf'} mass={10} />
+      <Suspense fallback="null">
+        <Hud />
+        <ModalBox title={'Bienvenue sur Nature&You'} />
+      </Suspense>
+      <div className="scrollArea" {...events}>
+      <div style={{ position: 'sticky', top: 0 }} ref={domContent} />
+      </div>
+      <Canvas
+        id="canvas"
+        shadowMap
+        gl={{ alpha: false }}
+        onCreated={({ gl, events }) => {
+          // Export canvas events, we will put them onto the scroll area
+          setEvents(events)
+        }}>
+        <CameraTarget />
+        <hemisphereLight intensity={0.35} />
+        <spotLight position={[10, 10, 10]} angle={0.3} penumbra={1} intensity={2} castShadow />
+        <Sky
+          distance={3000}
+          turbidity={2}
+          rayleigh={4}
+          mieCoefficient={0.038}
+          mieDirectionalG={0.85}
+          sunPosition={[Math.PI, -10, 0]}
+          exposure={5}
+          azimuth={0.5}
+        />
+        <HTML center portal={domContent}>
+          <h2>section 2</h2>
+        </HTML>
+        <Physics>
+          <Models />
+          <Vehicle position={[-5, 5, 5]} rotation={[0, -Math.PI * 1.2, 0]} angularVelocity={[0, 0.5, 0]} />
+          <Ground mode="basic" scale={1} parallaxFactor={-0.2} minLayers={8} maxLayers={30} />
+          <Cube />
+        </Physics>
+      </Canvas>
+      <Loader />
     </>
   )
 }
 
-
-function Plane({ minLayers, maxLayers, parallaxFactor, mode, scale }) {
-  const [map, bumpMap] = useTexture(['/textures/floor3.jpg', '/textures/floorbump.jpg'])
-
-  map.wrapS = THREE.RepeatWrapping
-  map.wrapT = THREE.RepeatWrapping
-  map.offset.set(0, 0)
-  map.repeat.set(1500, 1500)
-  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }))
-
-  return (
-    <mesh ref={ref} receiveShadow ref={ref} receiveShadow>
-      <planeBufferGeometry attach="geometry" args={[200, 200]} />
-      <ParallaxMapMaterial map={map} bumpMap={bumpMap} mode={mode} parallaxScale={parallaxFactor} parallaxMinLayers={minLayers} parallaxMaxLayers={maxLayers} />
-    </mesh>
-  )
-}
-
-ReactDOM.render(
-  <>
-    <Suspense fallback="null">
-      <Hud />
-      <ModalBox title={'Bienvenue sur Nature&You'} />
-    </Suspense>
-    <Canvas id="canvas" shadowMap gl={{ alpha: false }}>
-      <CameraTarget />
-      <hemisphereLight intensity={0.35} />
-      <spotLight position={[10, 10, 10]} angle={0.3} penumbra={1} intensity={2} castShadow />
-      <Sky
-        distance={3000}
-        turbidity={2}
-        rayleigh={4}
-        mieCoefficient={0.038}
-        mieDirectionalG={0.85}
-        sunPosition={[Math.PI, -10, 0]}
-        exposure={5}
-        azimuth={0.5}
-      />
-      <Physics>
-        <Models />
-        <Vehicle position={[-5, 5, 5]} rotation={[0, -Math.PI * 1.2, 0]} angularVelocity={[0, 0.5, 0]} />
-        <Plane mode="basic" scale={1} parallaxFactor={-0.2} minLayers={8} maxLayers={30} />
-        <Cube />
-      </Physics>
-    </Canvas>
-    <Loader />
-  </>,
-  document.getElementById('root')
-)
+ReactDOM.render(<App />, document.getElementById('root'))
