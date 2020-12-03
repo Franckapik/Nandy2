605ec952a212ac486cb20226d546a43589be88b5
Franckapik <fanch44@hotmail.com>
2020-12-03 10:48:08 +0100

    Cube into references

--- src/references/Cube.js
+++ src/references/Cube.js
@@ -2,7 +2,7 @@ import { useBox } from '@react-three/cannon';
 import React from 'react';
 
 export function Cube(props) {
-  const [ref] = useBox(() => ({ mass: 1, position: [0, 5, 0], rotation: [0.4, 0.2, 0.5], ...props }));
+  const [ref] = useBox(() => ({ mass: 1, position: props.position, rotation: [0.4, 0.2, 0.5], ...props }));
   return (
     <mesh receiveShadow castShadow ref={ref}>
       <boxBufferGeometry attach="geometry" />
