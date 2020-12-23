11689775b7267d0969712938f1ec65f2c78ab67d
franckapik <fanch44@hotmail.com>
2020-12-23 23:00:35 +0100

    adding zone1 gltf

--- src/Tools/Models.js
+++ src/Tools/Models.js
@@ -6,8 +6,8 @@ export const Models = (props) => {
   const matcaps = useMatcaps('./matcaps/128/'); //load just once
   return (
     <>
-      <Model matcaps={matcaps} url={'/passive.gltf'} mass={0} />
-      <Model matcaps={matcaps} url={'/active.gltf'} mass={10} />
+      <Model matcaps={matcaps} url={'/passive1.gltf'} mass={0} />
+      <Model matcaps={matcaps} url={'/active1.gltf'} mass={10} />
     </>
   );
 };
