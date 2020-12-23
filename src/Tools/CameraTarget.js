235edaa3c47511e03c954937485b4e073523f336
franckapik <fanch44@hotmail.com>
2020-12-23 02:26:22 +0100

    changing Orbit for camera from drei

--- src/Tools/CameraTarget.js
+++ src/Tools/CameraTarget.js
@@ -1,49 +1,42 @@
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
 import React, {useRef} from 'react'
 import {useFrame, useThree} from 'react-three-fiber'
 import useStore from "../store";
 import { Vector3 } from "three";
+import useEmpty from '../hooks/useEmpty';
 
 
 export default function CameraTarget() {
 
-  const offset = new Vector3() //Orbit
-  const offset2 = new Vector3(0,2,8) //Fixed
-  const offset3 = new Vector3(0,1,-5) //Lookat offset
-  const ref = useRef();
+  const offset = new Vector3()
   const cameraControlsEnabled = useStore(state => state.cameraControlsEnabled)
   const cameraTarget = useStore(state => state.cameraTarget)
 
+  //calcul initial offset Vehicle-Camera from blender
+  const originCamera = useEmpty('originCamera') 
+  const originVehicle = useEmpty('origin1Character') 
+  const a = new Vector3(...originCamera)
+  const b = new Vector3(...originVehicle)
 
-  useFrame(({camera}) => {
-    if(cameraTarget && ref.current) {
-      const posTarget= new Vector3(...cameraTarget) //position of vehicle (=target lookAT)
-      offset.copy(camera.position).sub(ref.current.target) 
-      //distance camera-vehicle setted by OrbitControls.
-      posTarget.add(offset3)
-      ref.current.target.copy(posTarget) //look at the target = vehicle
-      camera.position.copy(posTarget).add(offset) //set camera position + offset
-    }
+  const moovingVehicle = new Vector3(...cameraTarget)
+  offset.copy(a).sub(b)
+  console.log(offset);
+  
+  const cameraRef = useRef()
+  const cam = useRef()
+
+  useFrame(() => {
+  cameraRef.current.position.copy(moovingVehicle)
+  cam.current.lookAt(moovingVehicle)
   })
-
-
+  
   return (
-    <OrbitControls
-      ref={ref}
-      enabled={cameraControlsEnabled}
-      enableRotate={false}
-      enableDamping
-      dampingFactor={0.1}
-      enableKeys={false}
-      enablePan
-      enableZoom
-      minDistance={15}
-      maxDistance={50}
-      zoomSpeed={5}
-      minPolarAngle={Math.PI/104}
-      maxPolarAngle={Math.PI/2.5}
-      mouseButtons = {{ LEFT : 2}}
-    />
-  );
+  <group ref={cameraRef}>
+    <PerspectiveCamera ref={cam} makeDefault fov={30} position={offset}/>
+  </group>
+  )
 }
+
+
+
 
