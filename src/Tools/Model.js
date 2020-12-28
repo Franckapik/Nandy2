01a05936c6712b80cc263a759c17c05339d89c9c
Franckapik <fanch44@hotmail.com>
2020-12-29 00:04:43 +0100

    map materials

--- src/Tools/Model.js
+++ src/Tools/Model.js
@@ -3,24 +3,18 @@ import React from "react";
 import { ObjMesh } from './ObjMesh';
 
 export const Model = ({ url, mass, matcaps }) => {
-  const { nodes } = useGLTF(url, '/draco/');
+  const { nodes, materials } = useGLTF(url, '/draco/');
   const first = Object.keys(nodes);
 
   return Object.entries(nodes).map(([name, obj]) => {
     if (obj.type === 'Group' && obj.name !== first[0]) {
-      return (
-        <group key={name}>
-          {Object.entries(obj.children).map(([name, obj]) => {
-            return <ObjMesh mat={matcaps} mass={mass} display={true} key={name} {...obj} position={obj.getWorldPosition()} />;
-          })}
-        </group>
-      );
+      return null
     } else if (obj.type === 'Mesh') {
       if (obj.parent.name === first[0]) {
         obj.geometry.computeBoundingBox();
         const b = obj.geometry.boundingBox.max;
         let bound = [b.x * 2, b.y * 2, b.z * 2]; //half extent ? And for mesh rotated?
-        return <ObjMesh mat={matcaps} mass={mass} display={true} key={name} {...obj} position={obj.getWorldPosition()} />;
+        return <ObjMesh mat={obj.material} mass={mass} display={true} key={name} {...obj} position={obj.getWorldPosition()} />;
       }
     } else {
       //do nothing
