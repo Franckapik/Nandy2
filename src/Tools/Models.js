3f5da21c594f04a6f79144f9d002ef5ff78a3634
Franckapik <fanch44@hotmail.com>
2021-01-10 21:57:06 +0100

    Flower in Single object

--- src/Tools/Models.js
+++ src/Tools/Models.js
@@ -1,27 +1,23 @@
-import React from 'react';
-import { Model } from './Model';
-import { useRandomFromNavmesh } from '../hooks/useRandomFromNavmesh';
-import { InstanciateMesh } from './InstanciateMesh';
-import { useGLTF } from '@react-three/drei';
-import { Bubble } from './Bubble';
-import { Cube } from '../references/Cube';
-import useStore from '../store';
+import React from 'react'
+import { Model } from './Model'
+import { useRandomFromNavmesh } from '../hooks/useRandomFromNavmesh'
+import { InstanciateMesh } from './InstanciateMesh'
+import { useGLTF } from '@react-three/drei'
+import { Bubble } from './Bubble'
+import { Cube } from '../references/Cube'
+import useStore from '../store'
 
-
-
-const Single = ({url, name, ...props}) => {
+const Single = ({ url, name, ...props }) => {
   const { nodes } = useGLTF(url, '/draco/')
-  const position=[]
+  const position = []
   nodes[name].getWorldPosition().toArray(position)
-  console.log(props);
-  return (
-    <mesh material={nodes[name].material} geometry={nodes[name].geometry} position={position} {...props} />
-  )
+  console.log(props)
+  return <mesh material={nodes[name].material} geometry={nodes[name].geometry} position={position} {...props} />
 }
 
 export const Models = (props) => {
-  const randomPositions = useRandomFromNavmesh('/navmesh_applied.glb', 1000, 3);
-  const toggleVisible = useStore(state => state.toggleVisible);
+  const randomPositions = useRandomFromNavmesh('/navmesh_applied.glb', 1000, 3)
+  const toggleVisible = useStore((state) => state.toggleVisible)
 
   return (
     <>
@@ -29,9 +25,9 @@ export const Models = (props) => {
       <Model url={'/active1.gltf'} mass={10} />
       <Model url={'/traversant1.gltf'} mass={0} collision={0} />
       <InstanciateMesh arrayOfPositions={randomPositions} meshUrl={'./traversant.glb'} nameMesh={'Herb'} maxNumber={1000} />
-      <Bubble position={[-65, 2, 65]} scale={30} Text={['Pont solitaire', <br />, 'il s’est trouvé un ami', <br />, 'le vent vagabond']}>
-      <Single url={'/onclick1.gltf'} name='Fleur' onClick={() => toggleVisible()} position={[8,0,3]} />
+      <Bubble position={[-65, 2, 50]} scale={30} Text={['Pont solitaire', <br />, 'il s’est trouvé un ami', <br />, 'le vent vagabond']}>
+        <Single url={'/onclick1.gltf'} name="Fleur" onClick={() => toggleVisible()} position={[8, 0, 3]} />
       </Bubble>
     </>
-  );
-};
+  )
+}
