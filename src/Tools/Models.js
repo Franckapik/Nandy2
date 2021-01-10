89bfda7db2f5b606bd5e818aae77bdc6159f8d16
Franckapik <fanch44@hotmail.com>
2021-01-10 19:20:48 +0100

    instances to Models

--- src/Tools/Models.js
+++ src/Tools/Models.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Model } from './Model';
-import { useMatcaps } from '../hooks/useMatcaps';
+import { useRandomFromNavmesh } from '../hooks/useRandomFromNavmesh';
+import { InstanciateMesh } from './InstanciateMesh';
 
 export const Models = (props) => {
+  const randomPositions = useRandomFromNavmesh('/navmesh_applied.glb', 1000, 3);
+
   return (
     <>
       <Model url={'/passive1.gltf'} mass={0} />
       <Model url={'/active1.gltf'} mass={10} />
       <Model url={'/traversant1.gltf'} mass={0} collision={0} />
       <Model url={'/onclick1.gltf'} mass={0} collision={0} />
+      <InstanciateMesh arrayOfPositions={randomPositions} meshUrl={'./traversant.glb'} nameMesh={'Herb'} maxNumber={1000} />
     </>
   );
 };
