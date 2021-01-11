fe593be0ea3289ce7e08f793d1cdc34cac5946b4
Franckapik <fanch44@hotmail.com>
2021-01-11 23:05:36 +0100

    les 10 fps outlined flower

--- src/Tools/Models.js
+++ src/Tools/Models.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Model } from './Model'
 import { useRandomFromNavmesh } from '../hooks/useRandomFromNavmesh'
 import { InstanciateMesh } from './InstanciateMesh'
@@ -6,13 +6,24 @@ import { useGLTF } from '@react-three/drei'
 import { Bubble } from './Bubble'
 import { Cube } from '../references/Cube'
 import useStore from '../store'
+import { EffectComposer, Outline } from "react-postprocessing"
+
 
 const Single = ({ url, name, ...props }) => {
   const { nodes } = useGLTF(url, '/draco/')
   const position = []
   nodes[name].getWorldPosition().toArray(position)
-  return <mesh material={nodes[name].material} geometry={nodes[name].geometry} position={position} {...props} />
-}
+  const ref = useRef()
+  return( 
+    <EffectComposer>
+    <Outline 
+    edgeStrength={0.3} // the edge strength
+    pulseSpeed={0.3} // a pulse speed. A value of zero disables the pulse effect
+    blur={true}
+    selection={[ref]} />
+    <mesh ref={ref} material={nodes[name].material} geometry={nodes[name].geometry} position={position} {...props} />
+  </EffectComposer>
+)}
 
 export const Models = (props) => {
   const [randomPositions, navPosition] = useRandomFromNavmesh('/navmesh.glb', 'NavMesh', 30)
@@ -25,7 +36,7 @@ export const Models = (props) => {
       <Model url={'/traversant1.gltf'} mass={0} collision={0} />
       <InstanciateMesh position={navPosition} arrayOfPositions={[randomPositions]} meshUrl={'./traversant.glb'} nameMesh={'Herb'} maxNumber={1000} />
       <Bubble position={[-65, 2, 50]} scale={30} Text={['Pont solitaire', <br />, 'il s’est trouvé un ami', <br />, 'le vent vagabond']}>
-        <Single url={'/onclick1.gltf'} name="Fleur" onClick={() => toggleVisible()} position={[8, 0, 3]} />
+          <Single url={'/onclick1.gltf'} name="Fleur" onClick={() => toggleVisible()} position={[8, 0, 3]} />
       </Bubble>
 {/*       <Single url={'/navmesh.glb'} name="NavMesh" position={navPosition} /> */}
     </>
