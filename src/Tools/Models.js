941ffd69b5009a68cb8f53be45b07e4d40dbc935
franckapik <fanch44@hotmail.com>
2021-01-22 21:54:19 +0100

    working MeshSurfaceSampler

--- src/Tools/Models.js
+++ src/Tools/Models.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { PositionalAudio } from '@react-three/drei'
+import React, { useEffect, useRef, useState } from 'react'
+import { PositionalAudio, useGLTF } from '@react-three/drei'
 import useEmpty from '../hooks/useEmpty'
 import { useRandomFromNavmesh } from '../hooks/useRandomFromNavmesh'
 /* import useToggle from '../hooks/useToggle'
@@ -7,20 +7,48 @@ import { useRandomFromNavmesh } from '../hooks/useRandomFromNavmesh'
 import { Model } from './Model'
 import Video from './Video'
 import { Frame } from './Frame'
+import { MeshSurfaceSampler } from 'three/examples/jsm/math/MeshSurfaceSampler'
+import { Vector3 } from 'three'
+
 
 const MeshOnNavMesh = ({navMeshUrl, nameNavMesh, meshUrl, nameMesh, maxNumber}) => {
-  console.log({navMeshUrl, nameNavMesh, meshUrl, nameMesh, maxNumber});
 
   const [randomPositions, navPosition] = useRandomFromNavmesh(navMeshUrl, nameNavMesh, 50) //warning : no draco!
   return(
     <InstanciateMesh position={navPosition} arrayOfPositions={[randomPositions]} meshUrl={meshUrl} nameMesh={nameMesh} maxNumber={maxNumber} />
   )
 }
+const useRandom = (url, name) => {
+  const { nodes } = useGLTF(url, '/draco/')
+  const [randomPos, setPos] = useState([])
+  console.log(nodes);
+  const plane = useRef();
+  const _position = new Vector3()
+  const random = []
+
+  useEffect(()=> {
+    const sampler = new MeshSurfaceSampler( nodes[name])
+    /* 	.setWeightAttribute( 'color' )
+      .build(); */
+      console.log(sampler); 
+      sampler.build()
+
+      for ( let i = 0; i < 50; i ++ ) {
+        sampler.sample( _position );
+        random.push(_position)
+        setPos(oldArray => [...oldArray, _position] )
+      }
+
+    }, [plane])
+
+return randomPos
+}
 
 export const Models = (props) => {
   /*   const [isVisible, setVisible] = useToggle(true)
    */ const soufflePos = useEmpty('origin2Souffle')
-
+   const random = useRandom('/navmesh.glb', 'NavMesh' )
+  console.log(random);
   return (
     <>
       <Model url={'/passive1.gltf'} mass={0} />
@@ -49,7 +77,7 @@ export const Models = (props) => {
       <Trash /> */}
       <Frame />
       <Video url={'/souffle.webm'} rotation={[0, -Math.PI, 0]} position={soufflePos} />
-
+      
       <PositionalAudio url={'/lisbon.mp3'} distance={0} loop={true} />
     </>
   )
