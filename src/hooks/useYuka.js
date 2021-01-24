0380205621f7f21abd131e90a589e2da49e91d69
Franckapik <fanch44@hotmail.com>
2021-01-24 21:35:22 +0100

    working target on navmesh change with useFrame

--- src/hooks/useYuka.js
+++ src/hooks/useYuka.js
@@ -7,30 +7,82 @@ import { useRandomFromNavmesh } from './useRandomFromNavmesh'
 
 const context = createContext()
 
+/* const FlowerGen = (props) => {
+
+  function setFlowerPos(min, max) {
+    let plusOrMinus = Math.random() < 0.5 ? -1 : 1
+    let distance = (Math.random() * (max - min) + min).toFixed(2) * plusOrMinus
+    return distance
+  }
+
+  const [count, setCount] = useState([0, 0, 0])
+  const [randomTime, setTime] = useState(5)
+
+  const min = 5
+  const max = 20
+
+  let elapsed = 0
+  useFrame(({ clock }, delta) => {
+    if (elapsed >= randomTime) {
+      setTime(Math.random() * (max - min) + min);
+      let randomPos = [setFlowerPos(2, 5), 0, setFlowerPos(2, 5)]
+      const chassisPos = useStore.getState().cameraTarget //get Store once
+      let offset = new Vector3(...randomPos);
+      let flowerPos = new Vector3();
+      let chassisV = new Vector3(...chassisPos)
+      flowerPos.copy(chassisV).add(offset)
+      setCount((oldArr) => [...oldArr, [flowerPos.x,flowerPos.y,flowerPos.z]])
+      elapsed = 0
+    } else {
+      elapsed += delta
+    }
+  })
+
+  return count.map((a, i) => {
+    return <Cube key={i} position={a} />
+  })
+} */
+
+
 export function Manager({ children, behavior }) {
   /*   setTimeout(() => generateTarget(target), 3000)
    */
+  const [targetPos, setPos] = useState()
   const [random] = useRandom('/navmesh.glb', 'NavMesh', 50)
 
   const [mgr] = useState(() => new EntityManager(), [])
   useStore.setState({ IAManager: mgr })
   const IAManager = useStore((state) => state.IAManager)
+  const target = IAManager.entities.find((item) => item.name === 'Target')
+  const vehicle = IAManager.entities.find((item) => item.name === 'Vehicle')
+
 
   useEffect(() => {
     if (random.length) {
-      const vehicle = IAManager.entities.find((item) => item.name === 'Vehicle')
-      const target = IAManager.entities.find((item) => item.name === 'Target')
-      const randomArr = random[Math.floor(Math.random() * random.length)]
-      target.position.set(randomArr[0], randomArr[1], randomArr[2])
-      const behavior2 = new SeekBehavior(target.position)
+      const behavior2 = new SeekBehavior(targetPos)
       if (vehicle) {
-        console.log('ici')
         vehicle.steering.add(behavior2)
       }
     }
-  }, [random]) //no dependencies
+  }, [targetPos]) //no dependencies
 
-  useFrame((state, delta) => IAManager.update(delta))
+  let elapsed = 0
+  let interval = 8
+
+  useFrame((state, delta) =>{ 
+    if (elapsed >= interval) {
+      const randomArr = random[Math.floor(Math.random() * random.length)]
+      target.position.set(randomArr[0], randomArr[1], randomArr[2])
+      setPos(target.position)
+      console.log(targetPos);
+
+      elapsed = 0
+    } else {
+      elapsed += delta
+    }
+
+
+    IAManager.update(delta)})
 
   return children
 }
